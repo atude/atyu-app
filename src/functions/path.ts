3990bcc74dbbd5d214e3614adf6914c942e75dcf
atude <mozamel.anwary1@gmail.com>
2022-07-29 18:47:51 +1000

    fix paths

--- src/functions/path.ts
+++ src/functions/path.ts
@@ -1,6 +1,6 @@
 import { homeDir, isMac } from "./commands/shell";
 
-export const atyuDir = `${homeDir}/.atyu/`;
+export const atyuDir = `~/.atyu/`;
 export const atyuQmkDir = `${atyuDir}qmk_firmware/`;
 export const atyuHomeConfigFilePath = `${atyuDir}qmk_firmware/atyu_home.json`;
 export const atyuKeyboardConfigFilename = "atyu_config.json";
@@ -9,4 +9,6 @@ export const atyuHConfigFilename = "atyu.h";
 export const getKeyboardDir = (keyboardDir: string) =>
   `${atyuQmkDir}${keyboardDir}`;
 
-export const pathOf = (path: string) => isMac ? path : path.split("/").join("\\");
+export const pathOf = (path: string) => isMac ? 
+  path.replace("~", homeDir) : 
+  path.replace("~", homeDir).split("/").join("\\");
